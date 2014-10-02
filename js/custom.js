a1974913ab697ac38b2ece03089b2fd3d910d25d
prynksethi <prynksethi@gmail.com>
2014-10-02 15:45:42 +0530

    updated

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,23 @@
-h
+$(window).scroll(function () {
+  if ( $(this).scrollTop() > 400 && !$('header').hasClass('open') ) {
+    $('header').addClass('open');
+    $('header').slideDown();
+   } else if ( $(this).scrollTop() <= 400 ) {
+    $('header').removeClass('open');
+    $('header').slideUp();
+  }
+});
+
+$( document ).ready(function() {
+$('.section2 nav a').click(function(event) {
+  event.preventDefault();
+
+  var target = $(this).attr('href');
+
+  // the .not() makes it so if we are on that page, it doesn't fade in and out the same content
+  $('.content:visible').not(target).fadeOut('slow', function() {
+    // after we fade out old content, fade in new
+    $(target).fadeIn('slow');
+  });
+});
+});
