d9130fe89b535d1c1be6387ab4c95ff2a49306ad
prynksethi <prynksethi@gmail.com>
2014-10-07 15:58:05 +0530

    sprites done

--- js/custom.js
+++ js/custom.js
@@ -30,10 +30,30 @@ $('.section2 nav a').click(function(event) {
           mapTypeId: google.maps.MapTypeId.ROADMAP
         }
         var map = new google.maps.Map(mapCanvas, mapOptions)
+		
+		  var contentString = '<div id="content">'+
+                '<div id="siteNotice">'+
+                '</div>'+
+                '<h5 id="firstHeading" class="firstHeading">Infratab Banglore Pvt Ltd.</h5>'+
+                '<div id="bodyContent">'+
+                '<p><br>Door No:386/5, 1st A Cross,<br> 7th Block, Koramangala,<br> Bangalore â€“ 560095.<br> Phone: (+91) 80 6454 3666.<br> Website: <a href="http://infratab.in" target="_blank">http://infratab.in</a></p>'+
+                '</div>'+
+                '</div>';
+            var infowindow = new google.maps.InfoWindow({
+                content: contentString
+            });
+			
+		
 		var point= new google.maps.LatLng(12.935368, 77.613104)
 		var marker=new google.maps.Marker({
 		position: point,
 		map:map
-		})
+		});
+		
+		 google.maps.event.addListener(marker, 'click', function() {
+    infowindow.open(map,marker);
+  });
+		
+		   
       }
       google.maps.event.addDomListener(window, 'load', initialize);
