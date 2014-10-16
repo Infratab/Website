70997a5d934b17766e00d43014c8520f5ccd6812
prynksethi <prynksethi@gmail.com>
2014-10-17 00:06:11 +0530

    new code

--- js/custom.js
+++ js/custom.js
@@ -1,28 +1,4 @@
-$(window).scroll(function () {
-  if ( $(this).scrollTop() > 400 && !$('header').hasClass('open') ) {
-    $('header').addClass('open');
-    $('header').slideDown();
-   } else if ( $(this).scrollTop() <= 400 ) {
-    $('header').removeClass('open');
-    $('header').slideUp();
-  }
-});
-
-$( document ).ready(function() {
-$('.section2 nav a').click(function(event) {
-  event.preventDefault();
-
-  var target = $(this).attr('href');
-
-  // the .not() makes it so if we are on that page, it doesn't fade in and out the same content
-  $('.content:visible').not(target).fadeOut('slow', function() {
-    // after we fade out old content, fade in new
-    $(target).fadeIn('slow');
-  });
-});
-});
-
- function initialize() {
+function initialize() {
         var mapCanvas = document.getElementById('map_canvas');
         var mapOptions = {
           center: new google.maps.LatLng(12.935368, 77.613104),
