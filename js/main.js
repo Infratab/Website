06699e52bff7f3148d7cd02de976044c14cad06a
priyanka0901 <gupta.priyanka0901@gmail.com>
2017-06-01 16:59:27 +0530

    Updated video section modal box

--- js/main.js
+++ js/main.js
@@ -12,3 +12,10 @@ if($('.navbar').length > 0){
         checkScroll();
     });
 }
+
+//modal box
+$(document).ready(function(){
+  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+  $('.modal').modal();
+});
+$('#modal1').modal('open');
