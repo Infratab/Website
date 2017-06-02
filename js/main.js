edd1c1681a21ee1b664d101105f8d988ed3fafd9
priyanka0901 <gupta.priyanka0901@gmail.com>
2017-06-02 18:01:34 +0530

    Added counter value function

--- js/main.js
+++ js/main.js
@@ -37,3 +37,19 @@ $(document).ready(function($) {
    });
 });
 
+//counter
+
+$(document).ready(function(){
+    var totalFoodWastage = 1300000000 ;
+    var totalSec = 31536000;
+    var averageFoodWastage = totalFoodWastage/totalSec;
+    console.log(averageFoodWastage);
+    var seconds = 0;
+    setInterval(function(){
+        seconds++;
+        $('#jsFoodSeconds').text(seconds);
+        var foodWastage = Number(parseInt(seconds * averageFoodWastage)).toLocaleString();
+        $('#jsAverageFood').text(foodWastage);
+    },1000);
+
+});
