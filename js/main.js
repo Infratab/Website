f2ea20e06b373bb35ebcd2d3e3df4afd32a4f642
rohinsha <r2hinsha@gmail.com>
2017-06-06 06:33:54 -0700

    Team page fixes resolve

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function($) {
 	function checkScroll(){
-	    var startY = $('.scroll').height() - 200  ; //The point where the navbar changes in px
+	    var startY = $('.scroll').height() - 50  ; //The point where the navbar changes in px
 	    if($(window).scrollTop() > startY){
 	        $('.navbar').addClass("scrolled");
 	    }else{
