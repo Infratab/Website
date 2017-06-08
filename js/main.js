f493ca9f85c46dda1a5452bd1b330b1d8ce160d3
priyanka1110 <priyankak@infratab.in>
2017-06-08 20:09:43 +0530

    Style know us section

--- js/main.js
+++ js/main.js
@@ -12,4 +12,19 @@ $(document).ready(function($) {
 	        checkScroll();
 	    });
 	}
+
+	/*Food wastage counter */
+var totalFoodWastage = 1300000000 ;
+var totalSec = 31536000;
+var averageFoodWastage = totalFoodWastage/totalSec;
+console.log(averageFoodWastage);
+var seconds = 0;
+setInterval(function(){
+		seconds++;
+		$('#foodSeconds').text(seconds);
+		var foodWastage = Number(parseInt(seconds * averageFoodWastage)).toLocaleString();
+		$('#averageFood').text(foodWastage);
+
+},1000);
+
 });
