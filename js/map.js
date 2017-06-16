93033d8623746ed99445bdfe1346f1eb6c0aab22
priyanka1110 <priyankak@infratab.in>
2017-06-16 14:39:31 +0530

    Move map js code from html file into different file

---
+++ js/map.js
@@ -0,0 +1,49 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiaW5mcmF0YWIiLCJhIjoiY2ozemhwbWxxMDNmNDJ3bGJxZjVkZzhzdyJ9.QA-mRtYzEfP0lZ0wX4WLIA';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/light-v9',
+    zoom: 1.5
+});
+
+map.on('load', function () {
+
+    map.addLayer({
+        "id": "points",
+        "type": "symbol",
+        "source": {
+            "type": "geojson",
+            "data": {
+                "type": "FeatureCollection",
+                "features": [{
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [77.585906, 12.912864]
+                    },
+                    "properties": {
+                        // "title": "Infratab",
+                        "icon": "marker"
+                    }
+                }, {
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [-119.10619300000002, 34.143765]
+                    },
+                    "properties": {
+                        // "title": "Infratab Inc",
+                        "icon": "marker"
+                    }
+                }]
+            }
+        },
+        "layout": {
+            "icon-image": "{icon}-15",
+            "icon-size": 1.5,
+            "text-field": "{title}",
+            "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+            "text-offset": [0, 0.6],
+            "text-anchor": "top"
+        }
+    });
+});
