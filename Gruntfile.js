a75734bf8016b7463e69a2bba627b34fed3cd517
priyanka1110 <priyankak@infratab.in>
2017-06-30 15:54:22 +0530

    Add grunt uglify for javascript

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,12 +11,25 @@ module.exports = function (grunt) {
             }
           }
       },
+      
       watch: {
         files: ["sass/**/*.scss"],
         tasks: ["sass"]
+      },
+      
+      uglify: {
+        development: {
+          files: {
+            'js/main.min.js': ["js/jquery-2.1.1.min.js", "js/materialize.min.js", "js/init.js", "js/main.js"]
+          }
+        }
       }
+
     });
 
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask( "default", ["sass", "uglify"]);
 };
