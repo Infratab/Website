d53d9f61633ab584cb370d8381adf432c4dcf990
priyanka1110 <priyankak@infratab.in>
2017-08-14 14:28:47 +0530

    Remove unwanted code

--- js/foodcounter.js
+++ js/foodcounter.js
@@ -1,29 +1,17 @@
 $(document).ready(function($) {
 
-	/*Food wastage counter */
-	var totalFoodWastage = 1300000000;
+	var totalFoodWastage = 1300000000 ;
 	var totalSec = 31536000;
 	var averageFoodWastage = totalFoodWastage/totalSec;
+	console.log(averageFoodWastage);
 	var seconds = 0;
-	var foodCounterOffset = Math.floor($('#foodCounter').offset().top);
-	var counterTrigger = true;
+	setInterval(function(){
+		seconds++;
+		$('#foodSeconds').text(seconds);
+		var foodWastage = Number(parseInt(seconds * averageFoodWastage)).toLocaleString();
+		$('.averageFood').text(foodWastage);
 
-	function foodCounter() {
-		setInterval(function(){
-			seconds++;
-			$('#foodSeconds').text(seconds);
-			var foodWastage = Number(parseInt(seconds * averageFoodWastage)).toLocaleString();
-			$('.averageFood').text(foodWastage);
-		},1000);
-	}
-
-	$(window).scroll(function() {
-		var windowOffset = $(window).scrollTop();
-		if((windowOffset >= foodCounterOffset - 500) && counterTrigger) {
-			counterTrigger = false;
-			foodCounter();
-		}
-	})
+	},1000);
 
 });
 
