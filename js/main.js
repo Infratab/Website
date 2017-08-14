c77f6499d59ae2c258e236b3710cc935d3342615
priyanka1110 <priyankak@infratab.in>
2017-08-14 14:24:00 +0530

    Remove node modifier for developement

--- js/main.js
+++ js/main.js
@@ -13,31 +13,6 @@ $(document).ready(function($) {
 	    });
 	}
 
-	/*Food wastage counter */
-	var totalFoodWastage = 1300000000;
-	var totalSec = 31536000;
-	var averageFoodWastage = totalFoodWastage/totalSec;
-	var seconds = 0;
-	var foodCounterOffset = Math.floor($('#foodCounter').offset().top);
-	var counterTrigger = true;
-
-	function foodCounter() {
-		setInterval(function(){
-			seconds++;
-			$('#foodSeconds').text(seconds);
-			var foodWastage = Number(parseInt(seconds * averageFoodWastage)).toLocaleString();
-			$('.averageFood').text(foodWastage);
-		},1000);
-	}
-
-	$(window).scroll(function() {
-		var windowOffset = $(window).scrollTop();
-		if((windowOffset >= foodCounterOffset - 500) && counterTrigger) {
-			counterTrigger = false;
-			foodCounter();
-		}
-	})
-
 	function infratabVideo(state) {
 		var div = document.getElementById("popupVideo");
 		var iframe = div.getElementsByTagName("iframe")[0].contentWindow;
